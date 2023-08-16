403616bac0babb6b39c5c1fbf6518b6df9ef260f
GregTheDev <mapler98@gmail.com>
2023-08-16 12:57:15 -0500

    add comments

--- src/mod_a/object_mod/mod.rs
+++ src/mod_a/object_mod/mod.rs
@@ -1,10 +1,10 @@
+//export all public items of submod_a
 pub mod submod_a;
 
+//selectively re-export public items of submod_b
 mod submod_b;
 pub use submod_b::func_b;
-
 
 struct ObjectA {
     pub field_a: u32,
 }
-
